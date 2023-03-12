f0f31da364f00f1ae55fe0306f971f1c9a93160e
Franklyn-Ukwueze <franklynifechukwu2@gmail.com>
2023-03-12 10:50:10 +0100

    working on pdf link retrieval

--- inec/request.py
+++ inec/request.py
@@ -1,82 +1,75 @@
-import os
+# for get the pdf files or url
 import requests
-from urllib.parse import urljoin
+
+# for tree traversal scraping in webpage
 from bs4 import BeautifulSoup
-import argparse
 
-#%% Example
-# one pdf
-#   python all_pdf_dl.py -l https://memento.epfl.ch/academic-calendar/ --save-here
-# many pdfs
-#   python all_pdf_dl.py -l https://idsc.ethz.ch/education/lectures/recursive-estimation.html
+# for input and output operations
+import io
 
-#%% TODO
-# rewrite as a function [okay]
-# add argparse [okay]
-#   print name all note
-#   set save folder
-#   TODO: download all files
-# update running examples [okay]
-# TODO: merge files with the same name
-# TODO: save subfoldname as webpagename -> change all webpage as underscore case
-# TODO: update to other type of files using class
-# TODO: download files with name containing non-latin characters
-# TODO: add tqdm viz
-# TODO: add file selection
-# TODO: add log-in
-# TODO: modify according to style guide
+# For getting information about the pdfs
+from PyPDF2 import PdfFileReader
 
-#%% Functions
-def all_pdf_download(args):
-    base_url = args.link
-    if args.save_here:
-        folder_path = os.getcwd()
-    else:
-        folder_path = args.folder_path
-        if not os.path.exists(args.folder_path):os.mkdir(args.folder_path)
-    print("====== 1. Set savepath: {} ======".format(folder_path))
-    print("====== 2. Start searching ======")
-    #response = requests.get(base_url)
-    response = requests.get(base_url, headers={'User-Agent': 'Custom'})
-    soup= BeautifulSoup(response.text, "html.parser")
-    search_res = soup.select("a[href$='.pdf']")
-    print("{} files found!!!".format(len(search_res)))
-    print("====== 3. Start downloading ======")
-    for counter, link in enumerate(search_res):
-        #Name the pdf files using the last portion of each link which are unique in this case
-        filename = link['href'].split('/')[-1]
-        file_save_path = os.path.join(folder_path,link['href'].split('/')[-1])
-        if args.print_all:
-            print("[{}/{}] {}".format(counter+1, len(search_res), filename))
-        with open(file_save_path, 'wb') as f:
-            f.write(requests.get(urljoin(base_url,link['href'])).content)
-    print("====== 4. Finished!!! ======")
+# website to scrap
+url = "https://www.inecelectionresults.ng/elections/latest"
+ 
+# get the url from requests get method
+read = requests.get(url)
+ 
+# full html content
+html_content = read.content
+ 
+# Parse the html content
+soup = BeautifulSoup(html_content, "html.parser")
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Test argparse')
-    ####################################
-    ############ ALL OPTION ############
-    ## Main option
-    # -l/--link
-    parser.add_argument('-l', '--link', required=True, type=str,
-                        help='write down site name')
-    # --print-all
-    parser.add_argument('--print-all', dest='print_all', action='store_true',
-                        help="print all filename")
-    parser.set_defaults(print_all=True)
-    # --save-here
-    parser.add_argument('--save-here', dest='save_here', action='store_true',
-                        help="save files here")
-    parser.set_defaults(save_here=False)
-    # --save--folder
-    # default setting -> Downloads/ in user’s home directory obtained by (os.path.expanduser('~'))
-    parser.add_argument('-f', '--folder_path', default=r""+os.path.join(os.path.expanduser('~'), "Downloads"), 
-                        type=str, help='save files in the given folder')
+# created an empty list for putting the pdfs
+list_of_pdf = set()
 
-    ########################################
-    ############ PARSING OPTION ############
-    args = parser.parse_args()
-    all_pdf_download(args)
+# accessed the first p tag in the html
+l = soup.find('p')
 
-#%% reference
-# from https://stackoverflow.com/questions/54616638/download-all-pdf-files-from-a-website-using-python
+# accessed all the anchors tag from given p tag
+p = l.find_all('a')
+
+# iterate through p for getting all the href links
+for link in p:
+	
+	# original html links
+	print("links: ", link.get('href'))
+	print("\n")
+	
+	# converting the extension from .html to .pdf
+	pdf_link = (link.get('href')[:-5]) + ".pdf"
+	
+	# converted to .pdf
+	print("converted pdf links: ", pdf_link)
+	print("\n")
+	
+	# added all the pdf links to set
+	list_of_pdf.add(pdf_link)
+
+
+def info(pdf_path):
+	response = requests.get(pdf_path)
+	
+	with io.BytesIO(response.content) as f:
+		pdf = PdfFileReader(f)
+		information = pdf.getDocumentInfo()
+		number_of_pages = pdf.getNumPages()
+
+	txt = f"""
+	Information about {pdf_path}:
+
+	Author: {information.author}
+	Creator: {information.creator}
+	Producer: {information.producer}
+	Subject: {information.subject}
+	Title: {information.title}
+	Number of pages: {number_of_pages}
+	"""
+	print(txt)
+	return information
+
+
+for i in list_of_pdf:
+	info(i)
