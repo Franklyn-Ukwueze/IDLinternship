404d38d9edc7fdfdd1e17454aaf17146c8f1b275
Franklyn-Ukwueze <franklynifechukwu2@gmail.com>
2023-09-25 03:19:35 +0100

    test script

--- apitest/work.py
+++ apitest/work.py
@@ -5,26 +5,35 @@ token = os.environ.get("ACCESS_TOKEN")
 username = os.environ.get("USERNAME")
 password = os.environ.get("PASSWORD")
 
-def get_api_response(url, request_type, payload=None):
-    headers = {"content-type": "text/plain"}
+def get_api_response(url, request_type, bearer_token, payload=None):
+    headers = {"content-type": "application/json", "Authorization": f"Bearer {bearer_token}"}
     
     try:
         #Make API call
         if request_type == "get":
-            r = requests.get(url=url, auth=HTTPBasicAuth(username, password), headers=headers, json=payload)
+            r = requests.get(url=url, headers=headers, json=payload)
         elif request_type == "post":
-            r = requests.post(url=url, auth=HTTPBasicAuth(username, password), headers=headers, json=payload)
+            r = requests.post(url=url, headers=headers, json=payload)
         elif request_type == "put":
-            r = requests.put(url=url, auth=HTTPBasicAuth(username, password), headers=headers, json=payload)
+            r = requests.put(url=url, headers=headers, json=payload)
 
             
         print(f"Status code: {r.status_code}")  #Print status code
         response = r.json()
+
+        # save_path = "equity_file.xlsx"
+        # if response.status_code == 200:
+        #     with open(save_path, 'wb') as file:
+        #         file.write(response.content)
+        #     print("File downloaded successfully.")
+        # else:
+        #     print(f"Failed to download file. Status code: {response.status_code}")
+
     except Exception as e:
         return f"Encountered error: {e}"
 
     print(response)
 
 payload = {}
-get_api_response("", "get")
+get_api_response("", "get", bearer_token="")
 
